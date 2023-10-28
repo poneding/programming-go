7afd62bf4d6389596e6a2a6950ae6ae7a56e548c
poneding <poneding@gmail.com>
2023-10-28 22:08:14 +0800

    add statemachine

---
+++ go-in-action/statemachine/go.mod
@@ -0,0 +1,3 @@
+module statemachine
+
+go 1.21.3
---
+++ go-in-action/statemachine/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// 状态机
+// 1、StateMachine 结构，CurrentState, Transitions, Callback
+// 2、Transition 结构，FromState, ToState, Event
+func main() {
+	sm := NewStateMachine("", []Transition{
+		{From: "", To: "stopped", Event: "create"},
+		{From: "stopped", To: "running", Event: "start"},
+		{From: "running", To: "paused", Event: "pause"},
+		{From: "paused", To: "running", Event: "resume"},
+		{From: "running", To: "running", Event: "restart"},
+		{From: "running", To: "stopped", Event: "stop"},
+	}, func(fromState, toState string, args []any) error {
+		fmt.Printf("Transition from %s to %s with args %v\n", fromState, toState, args)
+		return nil
+	})
+
+	sm.Trigger("create")
+	sm.Trigger("start")
+	sm.Trigger("pause")
+	sm.Trigger("resume")
+	sm.Trigger("restart")
+	sm.Trigger("stop")
+}
